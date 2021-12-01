c1f617e1126e997e341360c906541198db0d0638
khangzxrr <khangzxrr@gmail.com>
2021-12-01 16:54:59 +0700

    finish basic node rendering

--- Simulate.js
+++ Simulate.js
@@ -1,78 +1,99 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, TextInput, View, Picker, Button, Text } from 'react-native';
+import React from 'react';
+import { StyleSheet, Dimensions } from 'react-native';
+import Canvas from 'react-native-canvas';
 
-export default function Simulate() {
-  const [selectedAlgorithm, setSelectedAlgorithm] = useState('bfs');
+export default function Simulate({ navigation, route }) {
+    const maxNodes = route.params.nodesCount;
+    const algorithm = route.params.algorithm;
 
-  const [nodesCount, setNodesCount] = useState('0');
+    const screenWidth = Dimensions.get('window').width;
+    const screenHeight = Dimensions.get('window').height;
 
-  const [warningText, setWarningText] = useState('');
+    let canvasContext = null; //canvas context
 
-  const onNodesCountInputChanged = (newNodesCount) => {
-    newNodesCount = newNodesCount.replace(/[^0-9]/g, '');
-    
-    if (parseInt(newNodesCount) > 49) {
-      console.log('there are too many node');
-      setWarningText('There are too many nodes, display may hard to watch\nlitmit: 50');
-    }else{
-      setWarningText('');
+    let nodes = []; //contain nodes information
+    let justify = []; //weight between nodes
+
+    const padding = 30;
+    const nodeSize = 20;
+
+    const isTooCloseToAnotherNode = (curX, curY) => {
+        for(let i = 0; i < nodes.length; i++){
+            var distance = Math.sqrt(Math.pow(curX - nodes[i].x, 2) + Math.pow(curY - nodes[i].y, 2));
+
+            if (distance <= padding + nodeSize){
+                return true;
+            }
+        }
+
+        return false;
     }
-    
-    setNodesCount(newNodesCount);
-  };
+
+    for (let i = 0; i < maxNodes; i++) {
+        var newX = Math.floor(Math.random() * (screenWidth - padding + 1));
+        var newY = Math.floor(Math.random() * (screenHeight - padding + 1));
+
+        while (isTooCloseToAnotherNode(newX, newY) || newX <= padding || newY <= padding){
+            newX = Math.floor(Math.random() * (screenWidth - padding + 1));
+            newY = Math.floor(Math.random() * (screenHeight - padding + 1));
+        }
+
+        nodes.push({ 
+            x: newX, 
+            y: newY,
+            label: i + 1
+        });
+    }
+
+    console.log(nodes.length);
+
+    const renderNodes = () => {
+        nodes.forEach((node) => {
+            canvasContext.beginPath();
+            canvasContext.fillStyle = 'red';
+
+            canvasContext.arc(node.x, node.y, nodeSize, 0, 2 * Math.PI);
+            canvasContext.fill();
+            
+        });
+    }
+
+    const handleCanvas = (canvas) => {
+        if (canvas == null) {
+            return;
+        }
+
+        canvas.width = screenWidth;
+        canvas.height = screenHeight;
+
+        const ctx = canvas.getContext('2d');
+        canvasContext = ctx;
+
+        renderNodes();
+    };
 
 
-  return (
-    <View style={styles.container}>
-      <Text>Setting up graph parameters</Text>
 
-      <Text style={styles.warning}>{warningText}</Text> 
-
-      <Picker
-        selectedValue={selectedAlgorithm}
-        onValueChange={(itemValue, itemIndex) => setSelectedAlgorithm(itemValue)}
-
-        style={{ width: 200, heigt: 100 }}
-      >
-        <Picker.Item label="BFS" value="bfs"></Picker.Item>
-        <Picker.Item label="DFS" value="dfs"></Picker.Item>
-        <Picker.Item label="Dijkstra" value="dijkstra"></Picker.Item>
-        <Picker.Item label="A*" value="astar"></Picker.Item>
-
-      </Picker>
-
-
-      <TextInput 
-        value={nodesCount} 
-        onChangeText={onNodesCountInputChanged} 
-        style={styles.input} 
-        placeholder='Enter how many nodes here' 
-        keyboardType='numeric' 
-        maxLength={2}
-      />
-
-      <Button title='Confirm' />
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <Canvas ref={handleCanvas} />
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  warning: {
-    color: 'orange'
-  },  
-  input: {
-    borderColor: 'grey',
-    borderWidth: 1,
-    borderRadius: 10,
-    padding: 10,
-    margin: 10
-  }
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    warning: {
+        color: 'orange'
+    },
+    input: {
+        borderColor: 'grey',
+        borderWidth: 1,
+        borderRadius: 10,
+        padding: 10,
+        margin: 10
+    }
 });
