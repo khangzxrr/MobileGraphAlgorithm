f8cd1e5d201e670c14d9bc0625602c8923836818
khangzxrr <khangzxrr@gmail.com>
2021-12-01 13:06:55 +0700

    Finish basic input

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View, Picker, Button } from 'react-native';
 
 export default function App() {
+  const [selectedAlgorithm, setSelectedAlgorithm] = useState('bfs');
+
+  const [nodesCount, setNodesCount] = useState('0');
+
+  const [warningText, setWarningText] = useState('hi');
+
+  const onNodesCountInputChanged = (newNodesCount) => {
+    newNodesCount = newNodesCount.replace(/[^0-9]/g, '');
+    
+    if (parseInt(newNodesCount) > 49) {
+      console.log('there are too many node');
+      setWarningText('There are too many nodes, display may hard to watch\nlitmit: 50');
+    }else{
+      setWarningText('');
+    }
+    
+    setNodesCount(newNodesCount);
+  };
+
+
   return (
     <View style={styles.container}>
       <Text>Setting up graph parameters</Text>
+      <Text style={styles.warning}>{warningText}</Text>
+      <Picker
+        selectedValue={selectedAlgorithm}
+        onValueChange={(itemValue, itemIndex) => setSelectedAlgorithm(itemValue)}
+
+        style={{ width: 200, heigt: 100 }}
+      >
+        <Picker.Item label="BFS" value="bfs"></Picker.Item>
+        <Picker.Item label="DFS" value="dfs"></Picker.Item>
+        <Picker.Item label="Dijkstra" value="dijkstra"></Picker.Item>
+        <Picker.Item label="A*" value="astar"></Picker.Item>
+
+      </Picker>
+
+
+      <TextInput value={nodesCount} onChangeText={onNodesCountInputChanged} style={styles.input} placeholder='Enter how many nodes here' keyboardType='numeric' />
+
+      <Button title='Confirm' />
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +56,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  warning: {
+    color: 'orange'
+  },  
+  input: {
+    borderWidth: 1,
+    padding: 10,
+    margin: 15
+  }
 });
